0acc5ecff4dd98d2d1e35a477c8f21d5e600af11
tabletop1 <dmhoang@asu.edu>
2016-02-22 00:11:53 -0700

    Initial version

---
+++ Assignment3/src/cse360assign3/Calculator.java
@@ -0,0 +1,58 @@
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** returns the value of private instance total
+	 * 
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** adds a value to the total
+	 * 
+	 * @param value		integer to add to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** subtracts a value from the total
+	 * 
+	 * @param value		integer to subtract from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** multiplies the total by a value
+	 * 
+	 * @param value		integer to multiply with the total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** divides the total by a value
+	 * 
+	 * @param value		integer to divide from the total
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** returns a history of all actions
+	 * 
+	 * @return an empty String
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ Assignment3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,53 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void totalTest() {
+		
+	}
+
+	@Test
+	public void addTest() {
+		
+	}
+	
+	@Test
+	public void subTest() {
+		
+	}
+	
+	@Test
+	public void multTest() {
+		
+	}
+	
+	@Test
+	public void divTest() {
+		
+	}
+	
+	@Test
+	public void historyTest() {
+		
+	}
+}
